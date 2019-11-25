52c0d8ff870ac7d9f7d60041d5d7ba5e9eac8453
milutin zeljkovic <milutin.zeljkovic@gmail.com>
2019-11-25 14:09:27 +0100

    migration

--- src/app/ClinicalCenter.php
+++ src/app/ClinicalCenter.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ClinicalCenter extends Model
 {
+    protected $fillable = [
+        'name'
+    ];
+
     public function clinicalCenterAdmins()
     {
         return $this->hasMany('App\ClinicalCenterAdmin');
