34d2350b80632b3a7146969981b9b7c51cc0cd8a
Ivan Cincurak <ivan.cincurak98@gmail.com>
2019-11-25 18:09:10 +0100

    change route to register medical staff

--- src/routes/api.php
+++ src/routes/api.php
@@ -20,7 +20,7 @@ Route::group([
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('register', 'Auth\AuthController@register');
-    Route::post('registerMedicalStaff', 'Auth\AuthController@registerMedicalStaff');
+    Route::post('register/staff', 'Auth\AuthController@registerMedicalStaff');
     Route::post('login', 'Auth\AuthController@login');
     Route::post('logout', 'Auth\AuthController@logout');
     Route::post('me', 'Auth\AuthController@me');
