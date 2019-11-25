830e0afba8029751caa27fcf673299ea28337552
Ivan Cincurak <ivan.cincurak98@gmail.com>
2019-11-25 18:01:15 +0100

    register for medical staff

--- src/routes/api.php
+++ src/routes/api.php
@@ -20,6 +20,7 @@ Route::group([
     'prefix' => 'auth'
 ], function ($router) {
     Route::post('register', 'Auth\AuthController@register');
+    Route::post('registerMedicalStaff', 'Auth\AuthController@registerMedicalStaff');
     Route::post('login', 'Auth\AuthController@login');
     Route::post('logout', 'Auth\AuthController@logout');
     Route::post('me', 'Auth\AuthController@me');
@@ -32,7 +33,7 @@ Route::group([
     'prefix' => 'patients'
 ], function ($router){
     
-    Route::get('asd', 'PatientsController@getClinicsPatients');
+    Route::get('clinic', 'PatientsController@getClinicsPatients');
     Route::get('','PatientsController@getPatients')->middleware('can:fetch,App\Patient');
     Route::get('accept/{id}', 'PatientsController@accept')->middleware('can:accept,App\Patient');
     Route::get('decline/{id}', 'PatientsController@decline')->middleware('can:decline,App\Patient');
