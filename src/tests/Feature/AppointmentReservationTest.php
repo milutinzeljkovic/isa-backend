db4c6a628053234c86b5b6e059614f06008863b8
milutin zeljkovic <milutin.zeljkovic@gmail.com>
2020-02-08 23:24:20 +0100

    adsa

--- src/tests/Feature/AppointmentReservationTest.php
+++ src/tests/Feature/AppointmentReservationTest.php
@@ -63,7 +63,7 @@ class AppointmentReservationTest extends TestCase
         $response = $this->withHeaders([
             'X-Header' => 'Value',
             'Authorization' => $bearer,
-        ])->json('POST', '/api/appointment/request/'.$doctor->id, ['date' => '2020-12-12 12:00:00', 'appointment_type' => $at->id]);
+        ])->json('POST', '/api/appointment/request112/'.$doctor->id, ['date' => '2020-12-12 12:00:00', 'appointment_type' => $at->id]);
         
         
         $response
