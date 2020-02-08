a05813bff9e0e70b205e994e3ce39371e38fb21e
milutin zeljkovic <milutin.zeljkovic@gmail.com>
2020-02-08 19:56:47 +0100

    change test

--- src/tests/Feature/AppointmentReservationTest.php
+++ src/tests/Feature/AppointmentReservationTest.php
@@ -11,6 +11,8 @@ use App\Appointment;
 use App\AppointmentType;
 use Carbon\Carbon;
 use App\Patient;
+use Illuminate\Support\Facades\Hash;
+
 
 
 
@@ -23,7 +25,43 @@ class AppointmentReservationTest extends TestCase
      */
     public function testExample()
     {
+        $user1 = new User;
+        $user1->email = 'doca@gmail.com';
+        $user1->name = 'doca_name';
+        $user1->last_name = 'doca_lastname';
+        $user1->ensurance_id = '64645754';
+        $user1->phone_number = '43256434';
+        $user1->address = 'address';
+        $user1->city = 'city';
+        $user1->state = 'state';
+        $user1->password = \Hash::make('pass123', 'password');
+        $user1->has_loggedin = 1;
+        $doctor = new Doctor();
+        $doctor->save();
+        $doctor->user()->save($user1);
+
         $patient = Patient::first();
+
+        if($patient == null){
+            $user = new User;
+            $user->email = 'patient@gmail.com';
+            $user->name = 'patient_name';
+            $user->last_name = 'patient_lastname';
+            $user->ensurance_id = '94859335';
+            $user->phone_number = '43256434';
+            $user->address = 'address';
+            $user->city = 'city';
+            $user->state = 'state';
+            $user->password = \Hash::make('pass123', 'password');
+            $user->has_loggedin = 1;
+            $patient = new Patient();
+            $patient->save();
+            $patient->user()->save($user);
+        }
+
+        
+
+
         $user = $patient->user()->first();
 
         $response = $this->withHeaders([
@@ -44,9 +82,31 @@ class AppointmentReservationTest extends TestCase
             'Authorization' => $bearer,
         ])->json('GET', '/api/clinics');
 
+        $c = new Clinic();
+        $c->name='name';
+        $c->address='adresa';
+        $c->description='dobra';
+        $c->clinical_center_id=1;
+        $c->save();
+
+        $c1 = new Clinic();
+        $c1->name='name1';
+        $c1->address='adresa1';
+        $c1->description='dobra1';
+        $c1->clinical_center_id=1;
+        $c1->save();
 
         $clinics = Clinic::all();
+        
         $appType = AppointmentType::first();
+        if($appType == null){
+            $appType= new AppointmentType();
+            $appType->name='pregled grla';
+            $appType->save();
+
+
+
+        }
 
         $this->assertTrue($appType != null);
 
