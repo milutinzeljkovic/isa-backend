54f0b01c9efd848cec6911c9c9bfe13e460184bb
milutin zeljkovic <milutin.zeljkovic@gmail.com>
2020-02-08 20:37:22 +0100

    bugs fixed

--- src/tests/Unit/PredefinedAppointmentReservationTest.php
+++ src/tests/Unit/PredefinedAppointmentReservationTest.php
@@ -20,13 +20,42 @@ class PredefinedAppointmentReservationTest extends TestCase
      */
     public function testExample()
     {
-        $user1 = '2';
-        $user2 = '5';
+        $user1 = '1';
+        $user2 = '2';
+        $user = new User;
+
+        $user->email = 'patient1@gmail.com';
+        $user->name = 'patient1_name';
+        $user->last_name = 'patient1_lastname';
+        $user->ensurance_id = '94859335';
+        $user->phone_number = '43256434';
+        $user->address = 'address';
+        $user->city = 'city';
+        $user->state = 'state';
+        $user->password = \Hash::make('password');
+        $user->has_loggedin = 1;
+        $patient = new Patient();
+        $patient->save();
+        $patient->user()->save($user);
+
+        $user->email = 'patient2@gmail.com';
+        $user->name = 'patient2_name';
+        $user->last_name = 'patient2_lastname';
+        $user->ensurance_id = '94859335';
+        $user->phone_number = '43256434';
+        $user->address = 'address';
+        $user->city = 'city';
+        $user->state = 'state';
+        $user->password = \Hash::make('password');
+        $user->has_loggedin = 1;
+        $patient = new Patient();
+        $patient->save();
+        $patient->user()->save($user);
 
         $appointment = DB::table('appointments')->where('id','1')->first();
         $this->updateAppointment($appointment, $user2);
         $this->updateAppointment($appointment, $user1);
-        $this->assertEquals('5', $appointment->patient_id);
+        $this->assertEquals('2', $appointment->patient_id);
 
     }
 
